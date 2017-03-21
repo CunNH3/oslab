8e9a8e18c22bfe1d717081505ea13c9ac65d57aa
Ubuntu 16.10 32bit <lingjiawei>
2017-03-21 15:15:07 +0800

    open automatic submit log record

--- kernel/irq/irq_handle.d
+++
@@ -1,5 +0,0 @@
-kernel/irq/irq_handle.o: kernel/irq/irq_handle.c \
- /usr/include/stdc-predef.h include/x86/x86.h include/x86/cpu.h \
- include/common.h include/types.h include/const.h include/assert.h \
- include/x86/memory.h include/x86/io.h include/game.h include/common.h \
- include/adt/linklist.h include/assert.h
