bbbe2f206aa5a3f5e51c8b7f2b7cee923d854958
Ubuntu 16.10 32bit <lingjiawei>
2017-03-21 21:28:08 +0800

    test

--- kernel/game/effect.d
+++
@@ -1,6 +0,0 @@
-kernel/game/effect.o: kernel/game/effect.c /usr/include/stdc-predef.h \
- include/game.h include/common.h include/types.h include/const.h \
- include/assert.h include/adt/linklist.h include/assert.h \
- include/common.h include/string.h include/adt/linklist.h \
- include/device/video.h include/types.h include/const.h include/x86/x86.h \
- include/x86/cpu.h include/x86/memory.h include/x86/io.h
--- kernel/lib/random.d
+++
@@ -1,1 +0,0 @@
-kernel/lib/random.o: kernel/lib/random.c /usr/include/stdc-predef.h
