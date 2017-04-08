3a3c3bca76fd7067c239862bdfddcdca70de8e00
Auto_commit <151220061@smail.nju.edu.cn>
2017-04-08 16:25:36 +0800

    >  compile game 151220061 lingjiawei Linux ubuntu 4.8.0-41-generic #44-Ubuntu SMP Fri Mar 3 15:26:20 UTC 2017 i686 i686 i686 GNU/Linux  16:25:36 up 14:27,  1 user,  load average: 0.68, 0.89, 0.93 3d11a8346831a4a923a396ab384638226444257

--- kernel/device/video.c
+++ kernel/device/video.c
@@ -1,9 +1,14 @@
-#include "../include/common.h"
-#include "../include/string.h"
 #include "../include/device/video.h"
-#include "../include/device/font.h"
+#include "../include/video_mode.h"
+#include "../include/string.h"
 
 uint8_t *vmem;
+
+void init_vmem_addr()
+{
+	struct ModeInfoBlock *MIB = (struct ModeInfoBlock*)(0x7e00);
+	vmem = (uint8_t *) MIB->physbase;
+}
 void init_vmem()
 {
 	memcpy(vmem, 0, SCR_SIZE);
