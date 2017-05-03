96dac61670c288277feededc746704f8129d1930
Auto_commit <151220061@smail.nju.edu.cn>
2017-05-03 23:33:58 +0800

    >  run qemu 151220061 lingjiawei Linux ubuntu 4.8.0-46-generic #49-Ubuntu SMP Fri Mar 31 13:56:17 UTC 2017 i686 i686 i686 GNU/Linux  23:33:58 up  4:05,  1 user,  load average: 0.58, 0.57, 0.45 8c81801ed475bf0fb083fa0891f3a44675490d

--- kernel/main.c
+++ kernel/main.c
@@ -10,7 +10,7 @@
 #include "include/disk.h"
 
 #define SECTSIZE 512
-#define GAME_OFFSET_IN_DISK (0x19000)
+#define GAME_OFFSET_IN_DISK (10 * 1024 * 1024)
 
 SegMan *mm_malloc(uint32_t, uint32_t, uint32_t);
 void set_tss_esp0(int);
@@ -24,8 +24,6 @@ void init_timer();
 void init_idt();
 void init_segment();
 void init_memory();
-void init_process();
-void init_vmem();
 //void add_irq_handle(int,void (*)(void));
 
 //void timer_event();
@@ -65,16 +63,22 @@ int kernel_main()
 	struct ProgramHeader *ph, *eph;
 	unsigned char *pa, *i;
 
-	elf = (struct ELFHeader*)0x19000;
-	//uint8_t buf[4096];
-	//elf = (struct ELFHeader*)buf;
-	//printk("the addr of the buf: 0x%x\n",(uint32_t)buf);
+	//elf = (struct ELFHeader*)0x190000;
+	uint8_t buf[4096];
+	elf = (struct ELFHeader*)buf;
+	printk("the addr of the buf: 0x%x\n",(uint32_t)buf);
 	
 	readseg((unsigned char*)elf, 4096, GAME_OFFSET_IN_DISK);
 
 
 	ph = (struct ProgramHeader*)((char *)elf + elf->phoff);
 	eph = ph + elf->phnum;
+	/*for(;ph < eph;ph++)
+	{
+		pa = (unsigned char*)ph->paddr; 
+		readseg(pa, ph->filesz,GAME_OFFSET_IN_DISK + ph->off);
+		for (i = pa + ph->filesz;i < pa + ph->memsz;*i ++ = 0);
+	}*/
 	SegMan *tmp[3];
 	int p_flag[2] = {0xa, 0x2};
 	int cnt = -1,_vaddr;
@@ -104,7 +108,7 @@ int kernel_main()
 	tf->eip = elf->entry;
 	tf->cs = SELECTOR_USER(tmp[SEG_USER_CODE]->gdt);
 	tf->ss = SELECTOR_USER(tmp[SEG_USER_DATA]->gdt);
-	tf->esp = 0x2000000 - tmp[1]->base + _vaddr;
+	tf->esp = 0x20000000 - tmp[1]->base + _vaddr;
 
 	asm volatile("movl %0, %%esp" : :"a"((int)tf));
 	asm volatile("popa");
